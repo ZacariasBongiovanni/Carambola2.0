b42634effd742df260ac21469bdcf8e81deaac44
ZacariasBongiovanni <zacariasbongiovanni@gmail.com>
2023-07-31 01:26:03 -0300

    Prueba6

--- public/assets/scenes/juego3.js
+++ public/assets/scenes/juego3.js
@@ -3,6 +3,14 @@ export default class juego3 extends Phaser.Scene {
     super("juego3");
   }
 
+ 
+  init({timer,score2,TirosHechos}){
+    this.timer = timer
+    this.score2 = score2
+    this.TirosHechos = TirosHechos
+
+  }
+
   create() {
     const map = this.make.tilemap({ key: "map3" });
 
@@ -27,7 +35,7 @@ export default class juego3 extends Phaser.Scene {
     console.log(objectosLayer);
 
     this.jugador = this.physics.add
-    .sprite(235, 125, "Cañon")
+    .sprite(700, 320, "Cañon")
     .setVelocity(0,0)
     .setMaxVelocity(0,0)
 
@@ -46,7 +54,10 @@ export default class juego3 extends Phaser.Scene {
         maxSize: 2,
       }); 
   
-    this.estrellas = this.physics.add.group();
+    this.estrellas = this.physics.add.group({
+      immovable: true,
+      allowGravity: false,
+    });
 
     let spawnPoint = map.findObject("objetos", (obj) => obj.name === "salida");
     console.log("spawn point salida ", spawnPoint);
@@ -55,6 +66,16 @@ export default class juego3 extends Phaser.Scene {
       .setCircle(200, 170, -5)
       .setMaxVelocity(0, 0)
       .setScale(0.1);
+
+      spawnPoint = map.findObject("objetos", (obj) => obj.name === "bomba");
+      console.log("spawn point bomba", spawnPoint);
+      this.bomba = this.physics.add
+        .sprite(spawnPoint.x, spawnPoint.y, "bomb")
+        .setScale(2.5)
+        .setVelocity(0, 400)
+        .setBounce(1)
+        .setCircle(7, 1, 1)
+        .setCollideWorldBounds(true);
 
 
     objectosLayer.objects.forEach((objData) => {
@@ -68,14 +89,16 @@ export default class juego3 extends Phaser.Scene {
     });
 
     this.add.image(400, 20, "barra").setScale(1);
-    this.score = 0;
+    this.score = 10;
     this.scoreText = this.add.text(20, 20, "Tiros Disponibles " + this.score, {
       fontSize: "20px",
       fontStyle: "bold",
       fill: "#000000",
     });
 
-    this.scoreText = this.add.text(600, 20, "Mapa " + this.score, {
+    
+
+    this.score1Text = this.add.text(600, 20, "Mapa 2", {
       fontSize: "20px",
       fontStyle: "bold",
       fill: "#000000",
@@ -88,17 +111,20 @@ export default class juego3 extends Phaser.Scene {
       loop: true,
     });
 
-    this.timer = 0;
+    this.timer++;
     this.timerText = this.add.text(350, 20, "Tiempo " + this.timer, {
       fontSize: "20px",
       fontStyle: "bold",
       fill: "#000000",
     });
 
+  
     this.cursors = this.input.keyboard.createCursorKeys();
 
     this.physics.add.collider(this.pelota, plataformaLayer);
     this.physics.add.collider(this.salida, plataformaLayer);
+    this.physics.add.collider(this.bomba, plataformaLayer);
+
     // this.physics.add.collider(this.pelota, dañoLayer, this.Daño, null, this);
     this.physics.add.collider(this.pelota, this.salida, this.win, null, this);
     this.physics.add.collider(this.estrellas, plataformaLayer);
@@ -106,6 +132,13 @@ export default class juego3 extends Phaser.Scene {
       this.pelota,
       this.estrellas,
       this.juntarestrellas,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.pelota,
+      this.bomba,
+      this.Daño,
       null,
       this
     );
@@ -130,60 +163,106 @@ export default class juego3 extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+
+    this.canShoot = true;
+  
+
   }
+  
 
   update(){
-    
-    if (this.input.keyboard.checkDown(this.cursors.space,250)){
-      this.jugador.setVelocity(0,0);
-      this.jugador.anims.play("space", true);
-      this.fire(this.jugador);
-    } 
 
-
-    if (this.cursors.left.isDown){
-      this.jugador.anims.play("left", true);
-      this.jugador.rotation--;
-      
-    }
-    else if (this.cursors.right.isDown){
-      this.jugador.anims.play("right", true);
-      this.jugador.rotation++;
-
-    } 
+      // Lógica para controlar cuándo el jugador puede disparar
+   if (this.input.keyboard.checkDown(this.cursors.space, 250) && this.canShoot) {
+   this.jugador.setVelocity(0, 0); // Detener al jugador temporalmente al disparar
+   this.jugador.anims.play("space", true);
+   this.fire(this.jugador);
+   this.canShoot = false; // El jugador no puede disparar hasta que la pelota se detenga o salga de la pantalla
+   this.TirosHechos++;
   
+   this.score--;
+   this.scoreText.setText(
+    "Tiros Disponibles " + this.score
+  );
+  }
+    
+  // Código para el movimiento del jugador
+      const velocidadMovimiento = 5;
+
+   if (this.cursors.up.isDown) {
+   this.jugador.rotation -= 0.05;
+   } else if (this.cursors.down.isDown) {
+   this.jugador.rotation += 0.05;
+   }
+
+   const velocidadX = Math.cos(this.jugador.rotation) * velocidadMovimiento;
+   const velocidadY = Math.sin(this.jugador.rotation) * velocidadMovimiento;
+
   } 
 
   win() {
-    this.scene.start("Ganar");
+    this.scene.start("juego4",{ timer: this.timer, TirosHechos: this.TirosHechos, score2:this.score2});
   }
 
   juntarestrellas(pelota, estrella) {
     estrella.disableBody(true, true);
+    this.score2++; // Incrementar el puntaje de estrellas recolectadas
+   
   }
-  Daño() {
-    this.scene.start("juego3");
-  }
+  Daño(pelota, bomba) {
+    
+    
+    bomba.destroy();
+    this.canShoot = true;
+    if(this.score === 0){
+      this.scene.start("Derrota")
+    }
+
+  } 
 
   fire(object){
-    let pelota = this.pelota.get(object.x+17, object.y-30);
-    if (pelota){
-      pelota.setActive(true);
-      pelota.setVisible(true);
-      pelota.setBounce(1);
-      pelota.setScale(0.1);
-      pelota.setCircle(150, -15, -25)
-      pelota.body.velocity.y = 150;
-      pelota.body.velocity.x = -150;
+    // Código para disparar la pelota
+
+    
       
-    }
+      const velocidadDisparoX = 500;
+      const velocidadDisparoY = 500;
+
+      // Calcula las componentes de velocidad horizontal y vertical en función de la rotación del jugador
+      const velocidadX = Math.cos(this.jugador.rotation) * velocidadDisparoX;
+      const velocidadY = Math.sin(this.jugador.rotation) * velocidadDisparoY;
+
+      // Crea la pelota en la posición del jugador y establece sus características
+       let pelota = this.pelota.get(object.x +10 , object.y -1);
+       if (pelota) {
+       pelota.setActive(true);
+       pelota.setVisible(true);
+       pelota.setBounce(1);
+       pelota.setScale(0.1);
+       pelota.setCircle(150, -15, -25);
+
+      // Establece las velocidades horizontal y vertical de la pelota para dispararla en la dirección del jugador
+      pelota.setVelocity(velocidadX, velocidadY);
+
+      pelota.setCollideWorldBounds(true);
+      pelota.setBounce(1, 1); // Ajusta el valor de rebote para controlar la colisión con los límites del mundo
+
+      // Cuando la pelota se detiene o sale de la pantalla, el jugador puede disparar nuevamente
+      pelota.on('animationcomplete', () => {
+      this.canShoot = true;
+      });
+  }
   }
 
   oneSecond() {
     this.timer++;
     this.timerText.setText("Tiempo " + this.timer);
     if (this.timer <= 0) {
-      /* this.gameOver = true; */
+     
     }
   }
+ 
+  
+
+ 
 }
